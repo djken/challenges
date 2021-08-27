e6152674009858fd1c13af552c5d412a8ef021cb
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2021-08-27 12:33:37 -0400

    ch002

--- challenges1.py
+++ challenges1.py
@@ -1,3 +1,10 @@
 # 001
+"""
 firstname = input('What is your first name: ')
 print('Hello', firstname)
+"""
+
+#002
+firstname = input('What is your first name: ')
+lastname = input('What is your last name: ')
+print('Hello', firstname, lastname)
